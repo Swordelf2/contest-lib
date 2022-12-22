f9626f7bdf515d320aad7df0c8eb9ebfa01cc71d
Swordelf <Swordelf@mail.ru>
2022-12-22 06:08:16 +0300

    Add `gcd()` math function

--- src/math.rs
+++ src/math.rs
@@ -1,3 +1,17 @@
+type UInt = u64;
+
+pub fn gcd(mut a: UInt, mut b: UInt) -> UInt {
+    if a < b {
+        std::mem::swap(&mut a, &mut b);
+    }
+    assert!(a > 0);
+    while b != 0 {
+        a %= b;
+        std::mem::swap(&mut a, &mut b);
+    }
+    a
+}
+
 pub fn log2(value: i32) -> i32 {
     assert!(value > 0);
     (32 - (value).leading_zeros() - 1) as i32
@@ -17,6 +31,27 @@ pub fn to_bits(mut val: u32) -> Vec<bool> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_gcd() {
+        assert_eq!(gcd(0, 1), 1);
+        assert_eq!(gcd(1, 0), 1);
+        assert_eq!(gcd(2, 1), 1);
+        assert_eq!(gcd(3, 1), 1);
+        assert_eq!(gcd(1, 3), 1);
+        assert_eq!(gcd(2, 3), 1);
+        assert_eq!(gcd(3, 4), 1);
+        assert_eq!(gcd(2, 4), 2);
+        assert_eq!(gcd(4, 2), 2);
+        assert_eq!(gcd(5, 3), 1);
+        assert_eq!(gcd(11, 3), 1);
+        assert_eq!(gcd(4, 18), 2);
+        assert_eq!(gcd(100, 10), 10);
+        assert_eq!(gcd(15, 35), 5);
+        assert_eq!(gcd(64, 48), 16);
+        assert_eq!(gcd(9, 6), 3);
+        assert_eq!(gcd(1000000000000, 1000000), 1000000);
+    }
 
     #[test]
     fn test_log2() {
